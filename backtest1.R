a9dd93e52074a78e473bd9d949f9ac2fc1bb21fb
Stan Mlekodaj <smlekodaj1@gmail.com>
2015-04-10 15:27:58 -0500

    quantmod stuff, largeCap datafile

--- backtest1.R
+++ backtest1.R
@@ -7,8 +7,11 @@ rm(list=ls())
 dataDir<-paste(Sys.getenv("HOME"),"/Rscripts/data/",sep="")
 # ticker data directory
 tickerDir <- paste(Sys.getenv("HOME"),"/Finance/earnings_database/all_close/ticker_data/",sep="")
-# daily file archive
+# daily file archive directory
 archiveDir <- paste(Sys.getenv("HOME"),"/Finance/earnings_database/all_close/daily_archive/",sep="")
+# datafiles
+largeCapFile <- "largecapsymbs.csv"
+usc401kFile <- "usc401kSymbs.csv"
 
 # read SP500 5yr data
 sp5yr<-read.csv(paste(dataDir,"/sp500_5yr.csv",sep=""))
@@ -16,15 +19,19 @@ sp5yr$Date<-as.Date(sp5yr$Date,format='%m/%d/%Y')       #format date
 sp5yrts<-xts(sp5yr[,-1],sp5yr$Date)     # time series object
 
 
+# Analysis 
+# cumulative log-returns - look for periodicity
+#
 # 
 # backtester outline:
-#         load price data (& others) as xts
-#         add marker & analysis columns
+#         load price data (& others) as xts - getSymbols()
+#         calculate returns                     - ClCl(), diff(log()), periodReturn()
+#         add marker & analysis columns. Account for calculation delay  - TTR package
 #         add long & short indicators. Buy/Sell when indicator transitions
 
 # Backtest on EOD close data
 # long/short ideas:
-#       bolinger bands
+#       bolinger bands, i.e. SMA +/- xSDDEVs
 #       bolinger bands vs. market, industry, sector
 #       RSI
 #       trends: EMA, SMA
@@ -32,6 +39,7 @@ sp5yrts<-xts(sp5yr[,-1],sp5yr$Date)     # time series object
 #       Gap
 #       USD/EUR
 #       other indexes USA & foreign
+# In order to use TTR package for indicators, need OHLC price objects (getSymbols)
 
 
 # Backtest on FinViz database
@@ -39,3 +47,20 @@ sp5yrts<-xts(sp5yr[,-1],sp5yr$Date)     # time series object
 #         analyst recomm, change
 #         earnings results (changes in forecast EPS)
 #         P/B, valuation
+
+# Example
+library(TTR)
+library(quantmod)
+
+# Get historical data for 19 large cap symbols listed in csv file, the save to datafile
+# largeCapSymbs<-as.character(read.csv(paste(dataDir,largeCapFile,sep=""),header=FALSE)[,1])
+# largeCap <- new.env()
+# largeCaps2007<-getSymbols(largeCapSymbs,env=largeCap,src="yahoo")
+# save(list = ls(largeCap), file = "largeCap2007",envir=largeCap)
+
+# load large cap historical quotes data file
+largeCap <- new.env()
+load(file ="largeCap2007",envir=largeCap)
+
+ATR(largeCap$AAPL["2014"])
+merge(largeCap$AAPL["2014"],ATR(largeCap$AAPL["2014"])) # add ATR indicator
