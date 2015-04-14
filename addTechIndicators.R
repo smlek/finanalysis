40bcd4377de1ee5e958c9d31c914b1a63f24bb6e
Stan Mlekodaj <smlekodaj1@gmail.com>
2015-04-14 16:48:10 -0500

    broken out addTechIndic from backtest

---
+++ addTechIndicators.R
@@ -0,0 +1,93 @@
+# Stan Mlekodaj
+# April 14, 2015
+#
+# Takes a list of stock symbols from a csv file and downloads historical data
+# from Yahoo Finance. Then adjusts Close prices for dividends/splits. Then adds
+# various technical indicators to each downloaded symbol and saves all results
+# into a R datafile.
+###############################################################################
+rm(list=ls())
+library(xts)
+library(TTR)
+library(quantmod)
+
+#########  FOLDER LOCATIONS  ##################################################
+# R data directory
+dataDir<-paste(Sys.getenv("HOME"),"/Rscripts/data/",sep="")
+# ticker data directory
+tickerDir <- paste(Sys.getenv("HOME"),"/Finance/earnings_database/all_close/ticker_data/",sep="")
+# daily file archive directory
+archiveDir <- paste(Sys.getenv("HOME"),"/Finance/earnings_database/all_close/daily_archive/",sep="")
+
+#########  INPUTS  ############################################################
+# csv stock symbol files
+symbFile <- "largecapsymbs.csv"
+usc401kFile <- "usc401kSymbs.csv"
+# Input R data files
+quoteFile <- "largeCap2007" # OHLC data for 19 US large caps. 2007-April 2015
+# SP500 5yr data
+sp5yr<-read.csv(paste(dataDir,"/sp500_5yr.csv",sep=""))
+sp5yr$Date<-as.Date(sp5yr$Date,format='%m/%d/%Y')       #format date
+sp5yrts<-xts(sp5yr[,-1],sp5yr$Date)     # time series object
+
+#########  OUTPUTS  ###########################################################
+# Output file
+quotesTechsFile <- "largeCap2007TechInd" # contains OHLC + tech indicators for 19 US large caps
+
+###############################################################################
+
+# Get historical data for symbols in symbFile list
+symbs<-as.character(read.csv(paste(dataDir,symbFile,sep=""),header=FALSE)[,1])
+envmt <- new.env()
+getSymbols(symbs,env=envmt,src="yahoo")
+#save(list = ls(envmt), file = quoteFile,envir=envmt) # save to datafile
+
+# load OHLC data file 
+#load(file = quoteFile,envir=envmt)
+
+#i<-ls(envmt)[1] # debugging
+for (i in ls(envmt)) {
+        # initialize indicators
+        atr14<-bbands202<-cmf20<-cmo14<-ema20<-vwma20<-macd12269<-rsi14<-
+                stoch1433<-wpr<-indicators<-curData<-
+                daylogret<-weeklogret<-monthlogret<-
+                list()
+        
+        curData <- adjustOHLC(get(i,envir=envmt),use.Adjusted=TRUE) #replace Close with Adjusted prices
+        #curData<-curData["2014"] # debugging
+        
+        # Returns
+        daylogret <- periodReturn(curData,period="daily", type="log")
+        weeklogret <- periodReturn(curData,period="weekly", type="log")
+        monthlogret <- periodReturn(curData,period="monthly", type="log")
+        
+        # Technical indicators
+        atr14 <- ATR(HLC(curData), n=14, maType="EMA") # Average True Range
+        bbands202 <- BBands(HLC(curData),n=20,sd=2) #Bollinger Bands
+        cmf20 <- CMF(HLC(curData),Vo(curData),n=20); names(cmf20) <- "cmf20" #Chaikin Money Flow
+        cmo14 <- CMO(Cl(curData),n=14); names(cmo14) <- "cmo14" # Chande Momentum Oscillator
+        ema20 <- EMA(Cl(curData),n=20,wilder = FALSE); names(ema20) <- "ema20" # Exponential moving average
+        vwma20 <- VWMA(Cl(curData),Vo(curData),n=20); names(vwma20) <- "vwma20" # Volume-weighed moving average
+        macd12269 <- MACD(Cl(curData), nFast = 12, nSlow = 26, nSig = 9, maType="EMA", percent = TRUE) # MACD
+        rsi14 <- RSI(Cl(curData),n=14,maType="SMA"); names(rsi14) <- "rsi14" # RSI
+        stoch1433 <- stoch(HLC(curData), nFastK = 14, nFastD = 3, nSlowD = 3) # Stochastic oscillator-overbought(oversold) if >80 (<20)
+        wpr14 <- WPR(HLC(curData), n = 14); names(wpr14) <- "wpr14" # William's %R - overbought(oversold) if <20 (>80)
+        indicators <- merge(curData, # add tech indicators columns to original price data
+                         atr14,
+                         bbands202,
+                         cmf20,
+                         cmo14,
+                         ema20,
+                         vwma20,
+                         macd12269,
+                         rsi14,
+                         stoch1433,
+                         wpr14
+                         )
+        # replace original data
+        assign(i,indicators,envir=envmt)
+}
+
+# save price & tech indicators into datafile
+save(list = ls(envmt), file = quotesTechsFile, envir=envmt)
+
--- backtest1.R
+++ backtest1.R
@@ -6,30 +6,40 @@ library(xts)
 library(TTR)
 library(quantmod)
 
+#########  FOLDER LOCATIONS  ##################################################
 # R data directory
 dataDir<-paste(Sys.getenv("HOME"),"/Rscripts/data/",sep="")
 # ticker data directory
 tickerDir <- paste(Sys.getenv("HOME"),"/Finance/earnings_database/all_close/ticker_data/",sep="")
 # daily file archive directory
 archiveDir <- paste(Sys.getenv("HOME"),"/Finance/earnings_database/all_close/daily_archive/",sep="")
-# datafiles
-largeCapFile <- "largecapsymbs.csv"
-usc401kFile <- "usc401kSymbs.csv"
 
-# read SP500 5yr data
+#########  INPUTS  ############################################################
+quotesTechsFile <- "largeCap2007TechInd" # contains OHLC + tech indicators for 19 US large caps
+# Input R data files
+#quoteFile <- "largeCap2007" # OHLC data for 19 US large caps. 2007-April 2015
+# SP500 5yr data
 sp5yr<-read.csv(paste(dataDir,"/sp500_5yr.csv",sep=""))
 sp5yr$Date<-as.Date(sp5yr$Date,format='%m/%d/%Y')       #format date
 sp5yrts<-xts(sp5yr[,-1],sp5yr$Date)     # time series object
 
+#########  OUTPUTS  ###########################################################
+
+###############################################################################
+
+
+# Load datafile with historical quotes & tech indicators into a new environment
+largeCap <- new.env()
+load(file = quotesTechsFile,envir=largeCap)
 
 # Analysis 
 # cumulative log-returns - look for periodicity
 #
 # 
 # backtester outline:
-#         load price data (& others) as xts - getSymbols()
+#         load price data (& others) as xts - getSymbols()                              DONE
 #         calculate returns                     - ClCl(), diff(log()), periodReturn()
-#         add marker & analysis columns. Account for calculation delay  - TTR package
+#         add marker & analysis columns. Account for calculation delay  - TTR package   DONE
 #         add long & short indicators. Buy/Sell when indicator transitions
 
 # Backtest on EOD close data
@@ -60,29 +70,4 @@ sp5yrts<-xts(sp5yr[,-1],sp5yr$Date)     # time series object
 # largeCaps2007<-getSymbols(largeCapSymbs,env=largeCap,src="yahoo")
 # save(list = ls(largeCap), file = "largeCap2007",envir=largeCap)
 
-# load large cap historical quotes data file
-largeCap <- new.env()
-load(file ="largeCap2007",envir=largeCap)
 
-envmt <- largeCap
-for (i in ls(envmt)) {
-        curData <- adjustOHLC(get(i,envir=envmt),use.Adjusted=TRUE) #replace Close with Adjusted
-        assign(i,curData,envir=envmt) 
-        curData<-curData["2014"] # debugging
-        ATR(HLC(curData), n=14, maType="EMA") # Average True Range
-        BBands(HLC(curData),n=20,sd=2) #Bollinger Bands
-        #When Chaikin Money Flow is above/below +/- 0.25 it is a bullish/bearish signal. 
-        #If Chaikin Money Flow remains below zero while the price is rising, it indicates a probable reversal.
-        CMF(HLC(curData),Vo(curData),n=20) #Chaikin Money Flow
-        CMO(Cl(curData),n=14) # Chande Momentum Oscillator
-        EMA(Cl(curData),n=20,wilder = FALSE) # Exponential moving average
-        VWMA(Cl(curData),Vo(curData),n=20) # Volume-weighed moving average
-        MACD(Cl(curData), nFast = 12, nSlow = 26, nSig = 9, maType="EMA", percent = TRUE)
-        RSI(Cl(curData),n=14)
-        stoch(HLC(curData), nFastK = 14, nFastD = 3, nSlowD = 3) # Stochastic oscillator-overbought(oversold) if >80 (<20)
-        WPR(HLC(curData), n = 14) # William's %R - overbought(oversold) if <20 (>80)
-}
-
-
-ATR(largeCap$AAPL["2014"])
-merge(largeCap$AAPL["2014"],ATR(largeCap$AAPL["2014"])) # add ATR indicator
